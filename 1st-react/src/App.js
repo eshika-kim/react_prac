f57ace6e5c8bc7feb5db39e8ea8deaaa34e6d22b
eshika.kim <eshika@naver.com>
2024-12-22 23:52:16 +0900

    Day5. create-react-app setting

---
+++ 1st-react/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+function App() {
+  return (
+    <div className="App">
+      <h1 className={styles.title}>Welcome Back!</h1>
+      <Button text={"continue"} />
+    </div>
+  );
+}
+
+export default App;
